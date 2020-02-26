3e20626f9946ae7f8726a6ebd8c77d4a6df9c187
dukebot <danielfg1990@gmail.com>
2020-02-26 20:51:54 +0100

    Added some comments on demo project.
    Also on the platformer tutorial changed the level template to be a true template.

--- DemoProject/Main.gd
+++ DemoProject/Main.gd
@@ -6,6 +6,7 @@ var score
 func _ready():
 	randomize()
 
+#Called when HUD emits the signal to start the game
 func new_game():
 	score = 0
 	$HUD.update_score(score)
@@ -14,6 +15,7 @@ func new_game():
 	$HUD.show_message("Get Ready")
 	$Music.play()
 
+#Called when player emits hit signal
 func game_over():
 	$DeathSound.play()
 	$Music.stop()
@@ -21,6 +23,12 @@ func game_over():
 	$MobTimer.stop()
 	$HUD.show_game_over()
 
+#The start timer runs the other timers
+func _on_StartTimer_timeout():
+	$MobTimer.start()
+	$ScoreTimer.start()
+
+#Called every time we have to spawn a mob
 func _on_MobTimer_timeout():
 	
 	# choose a random location on the Path2D
@@ -37,10 +45,7 @@ func _on_MobTimer_timeout():
 		Vector2(rand_range(mob.MIN_SPEED, mob.MAX_SPEED), 0).rotated(direction)
 	)
 
-func _on_StartTimer_timeout():
-	$MobTimer.start()
-	$ScoreTimer.start()
-
+#Called every time we have to add score
 func _on_ScoreTimer_timeout():
 	score += 1
 	$HUD.update_score(score)
--- DemoProject/Player.gd
+++ DemoProject/Player.gd
@@ -3,40 +3,63 @@ extends Area2D
 signal hit
 
 export (int) var SPEED
+
 var velocity = Vector2()
 var screensize
+
 
 func _ready():
 	hide()
 	screensize = get_viewport_rect().size
+
+
+func _on_Player_body_entered( body ):
+	$Collision.disabled = true
+	hide()
+	emit_signal("hit")
+
 
 func start(pos):
 	position = pos
 	show()
 	$Collision.disabled = false
 
+
 func _process(delta):
-	velocity = Vector2()
-	if Input.is_action_pressed("ui_right"):
-		velocity.x += 1
-	if Input.is_action_pressed("ui_left"):
-		velocity.x -= 1
-	if Input.is_action_pressed("ui_down"):
-		velocity.y += 1
-	if Input.is_action_pressed("ui_up"):
-		velocity.y -= 1
+	velocity = get_velocity()
+	position = update_position(position, velocity, delta, screensize)
+	animate(velocity)
+
+
+func get_velocity():
+	var out = Vector2()
+	
+	if Input.is_action_pressed("ui_right"): out.x += 1
+	if Input.is_action_pressed("ui_left"):  out.x -= 1
+	if Input.is_action_pressed("ui_down"):  out.y += 1
+	if Input.is_action_pressed("ui_up"):    out.y -= 1
+	
+	if out.length() > 0: out = out.normalized() * SPEED
+	
+	return out
+
+
+func update_position(current_position, velocity, delta, screensize):
+	var out = current_position
+	out += velocity * delta
+	out.x = clamp(out.x, 0, screensize.x)
+	out.y = clamp(out.y, 0, screensize.y)
+	return out
+
+
+func animate(velocity):
 	if velocity.length() > 0:
-		velocity = velocity.normalized() * SPEED
 		$AnimatedSprite.play()
 		$Trail.emitting = true
 	else:
 		$AnimatedSprite.stop()
 		$Trail.emitting = false
-
-	position += velocity * delta
-	position.x = clamp(position.x, 0, screensize.x)
-	position.y = clamp(position.y, 0, screensize.y)
-
+	
 	if velocity.x != 0:
 		$AnimatedSprite.animation = "right"
 		$AnimatedSprite.flip_v = false
@@ -45,10 +68,6 @@ func _process(delta):
 		$AnimatedSprite.animation = "up"
 		$AnimatedSprite.flip_v = velocity.y > 0
 
-func _on_Player_body_entered( body ):
-	$Collision.disabled = true
-	hide()
-	emit_signal("hit")
 
 
 
