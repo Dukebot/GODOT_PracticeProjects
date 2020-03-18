73bedca503986b1b2894f0247cc33a013a1d634f
dukebot <danielfg1990@gmail.com>
2020-03-18 21:51:25 +0100

    Enemy shooting

--- TopDown_TankBattle_YoutubeTutorial/tanks/EnemyTank.gd
+++ TopDown_TankBattle_YoutubeTutorial/tanks/EnemyTank.gd
@@ -5,11 +5,14 @@ onready var parent = get_parent()
 export (float) var turret_speed
 export (int) var detect_radius
 
+var speed = 0
 var target = null
 
 
 func _ready():
-	$DetectRadius/CollisionShape2D.shape.radius = detect_radius
+	var circle = CircleShape2D.new()
+	circle.radius = detect_radius
+	$DetectRadius/CollisionShape2D.shape = circle
 
 func _on_DetectRadius_body_entered(body):
 	#we've configurated that the DetecRadius Only detects the player
@@ -28,13 +31,23 @@ func _process(delta):
 		$Turret.global_rotation = current_direction.linear_interpolate(
 			target_direction, turret_speed * delta
 		).angle()
+		
+		#see dot product on godot documentation
+		if target_direction.dot(current_direction) > 0.9:
+			shoot()
 
 func control(delta):
+	if $LookAhead1.is_colliding() or $LookAhead2.is_colliding():
+		speed = lerp(speed, 0, 0.1)
+	else:
+		speed = lerp(speed, max_speed, 0.05)
+	
 	if (parent) is PathFollow2D:
 		parent.set_offset(parent.get_offset() + speed * delta)
 		position = Vector2() #make sure we are always on the path (parent position)
 	else:
 		#other tank movement code
+		
 		pass
 
 
