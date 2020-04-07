816b3a64ee3e3f722931118cf0e0d866bb15148a
DESKTOP-7Q6EFDB\Dani <danielfg1990@gmail.com>
2020-04-07 17:40:52 +0200

    Melee attack with hitboxes and hurtboxes

--- ActionRPG_YoutubeTutorial/World/Grass.gd
+++ ActionRPG_YoutubeTutorial/World/Grass.gd
@@ -1,13 +1,13 @@
 extends Node2D
 
-func _process(delta):
-	if Input.is_action_just_pressed("attack"):
-		var GrassEffect = load("res://Effects/GrassEffect.tscn")
-		
-		var grassEffect = GrassEffect.instance()
-		grassEffect.global_position = global_position
-		
-		var world = get_tree().current_scene
-		world.add_child(grassEffect)
-		
-		queue_free()
+func _on_HurtBox_area_entered(area):
+	create_grass_effect()
+	queue_free()
+
+func create_grass_effect():
+	var GrassEffect = load("res://Effects/GrassEffect.tscn")
+	var grassEffect = GrassEffect.instance()
+	grassEffect.global_position = global_position
+	
+	var world = get_tree().current_scene
+	world.add_child(grassEffect)
