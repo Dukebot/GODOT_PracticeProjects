700b20c142f07c8f7e76b39ec290f3edc0794a46
dukebot <danielfg1990@gmail.com>
2020-04-27 16:32:15 +0200

    Loony Lips: Getting text from the player and buttons

--- Loony_Lips_Udemy_Course/LoonyLips.gd
+++ Loony_Lips_Udemy_Course/LoonyLips.gd
@@ -1,8 +1,24 @@
 extends Control
 
+
 func _ready():
 	var prompts = ["Yann", "banana", "tingly", "glorious"]
 	var story = "One upon a time someone called %s and flavoured %s sandwich which made him feel all %s inside. It was %s day."
 	
 	var displayText = (story % prompts)
-	$DisplayText.text = displayText
+	$VBoxContainer/DisplayText.text = displayText
+
+
+func _on_PlayerText_text_entered(new_text):
+	update_DisplayText(new_text)
+
+func _on_TextureButton_pressed():
+	var words = $VBoxContainer/HBoxContainer/PlayerText.text
+	update_DisplayText(words)
+
+func update_DisplayText(words):
+	$VBoxContainer/DisplayText.text = words
+	$VBoxContainer/HBoxContainer/PlayerText.clear()
+
+
+
