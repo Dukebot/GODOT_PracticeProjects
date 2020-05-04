6d97dec5fc5d405d4c18b3c964c66e2bce56f97b
DESKTOP-7Q6EFDB\Dani <danielfg1990@gmail.com>
2020-05-04 19:27:50 +0200

    Hoppy Days: commiting things that left uncommited

--- Hoppy Days - Udemy Course/Player/Player.gd
+++ Hoppy Days - Udemy Course/Player/Player.gd
@@ -1,5 +1,19 @@
 extends KinematicBody2D
 
+const SPEED = 500
+
+var motion = Vector2(0, 0)
+
 
 func _ready():
 	pass
+
+
+func _physics_process(delta):
+	if Input.is_action_pressed("left") and not Input.is_action_pressed("right"):
+		motion.x = -SPEED
+	elif Input.is_action_pressed("right") and not Input.is_action_pressed("left"):
+		motion.x = SPEED
+	else:
+		motion.x = 0
+	move_and_slide(motion)
