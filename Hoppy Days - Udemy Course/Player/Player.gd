05d2541b06ca479d473f398782cc87de7aa59be7
DESKTOP-7Q6EFDB\Dani <danielfg1990@gmail.com>
2020-05-05 19:38:23 +0200

    Hoppy Days: Making the bunny jump. Note that gravity accel it's not multipliyed by delta, maybe this cause problem later, will need fuirther investigation.

--- Hoppy Days - Udemy Course/Player/Player.gd
+++ Hoppy Days - Udemy Course/Player/Player.gd
@@ -1,6 +1,9 @@
 extends KinematicBody2D
 
+const UP = Vector2(0, -1)
 const SPEED = 500
+const GRAVITY = 300
+const JUMP_SPEED = 3000
 
 var motion = Vector2(0, 0)
 
@@ -10,10 +13,30 @@ func _ready():
 
 
 func _physics_process(delta):
+	apply_gravity()
+	jump()
+	move()
+	move_and_slide(motion, UP)
+
+
+#I think this should be multipliyed by delta, as we are adding an acceleration amount each frame
+#If the framerate drops down we will be accelerating slower I think
+func apply_gravity():
+	if not is_on_floor():
+		motion.y += GRAVITY
+	else:
+		motion.y = 0
+
+
+func jump():
+	if Input.is_action_just_pressed("jump") and is_on_floor():
+		motion.y -= JUMP_SPEED
+
+
+func move():
 	if Input.is_action_pressed("left") and not Input.is_action_pressed("right"):
 		motion.x = -SPEED
 	elif Input.is_action_pressed("right") and not Input.is_action_pressed("left"):
 		motion.x = SPEED
 	else:
 		motion.x = 0
-	move_and_slide(motion)
