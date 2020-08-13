0fe3f1685999649414bd00e175b88e95158055f7
dukebot <danielfg1990@gmail.com>
2020-08-13 13:07:14 +0200

    Hoppy days: bug fixing (is_on_floor())

--- Hoppy Days - Udemy Course/Player/Player.gd
+++ Hoppy Days - Udemy Course/Player/Player.gd
@@ -25,7 +25,7 @@ func apply_gravity(delta):
 	if position.y > WORLD_LIMIT:
 		get_tree().call_group("GameState", "end_game")
 	
-	if is_on_floor():
+	if is_on_floor() and motion.y > 0:
 		motion.y = 0
 	elif is_on_ceiling():
 		motion.y = 1
@@ -51,11 +51,11 @@ func animate():
 func hurt():
 	position.y -= 1
 	yield(get_tree(), "idle_frame")
-	motion.y -= JUMP_SPEED
+	motion.y = -JUMP_SPEED
 	$HurtSFX.play()
 
 
 func boost():
 	position.y -= 1
 	yield(get_tree(), "idle_frame")
-	motion.y -= JUMP_SPEED * BOOST_MULTIPLIER
+	motion.y = -JUMP_SPEED * BOOST_MULTIPLIER
