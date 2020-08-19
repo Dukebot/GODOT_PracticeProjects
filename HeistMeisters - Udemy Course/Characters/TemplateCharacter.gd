7ad7d137df3e29c4d6e4ceb6e237129309a8f818
dukebot <danielfg1990@gmail.com>
2020-08-19 15:37:15 +0200

    Heist Meisters: Top down stealth game. Character template done.

---
+++ HeistMeisters - Udemy Course/Characters/TemplateCharacter.gd
@@ -0,0 +1,6 @@
+extends KinematicBody2D
+
+const SPEED = 10      #Acceleration per frame
+const MAX_SPEED = 100 #Max speed that player can move at given frame
+const FRICTION = 0.1  #Time to stop motion when not moving
+
