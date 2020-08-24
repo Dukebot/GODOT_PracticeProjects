0deabbf42a76612d89efe8d729fe801893efeb3c
dukebot <danielfg1990@gmail.com>
2020-08-24 23:53:56 +0200

    Platformer Engine: Adding basic enemy. Also added enemy detector for player as testing.

--- PlatformerEngine/Player/Player.gd
+++ PlatformerEngine/Player/Player.gd
@@ -1,13 +1,20 @@
 extends KinematicBody2D
 
+
 const UP = Vector2(0, -1)
-const ACCELERATION = 400
-const FRICTION = 400
-const GRAVITY = 800
+
+const ACCELERATION = 1000
+const FRICTION = 4000
+const GRAVITY = 2000
+
 const MAX_SPEED = 300
-const JUMP_SPEED = 500
+const JUMP_SPEED = 800
+
+const MAX_HEALTH = 1
+
 
 var motion = Vector2()
+var health = MAX_HEALTH
 
 
 func _ready():
@@ -15,26 +22,42 @@ func _ready():
 
 
 func _physics_process(delta):
-	
-	#Horizontal movement
+	horizontal_movement(delta)
+	apply_gravity(delta)
+	jump()
+	motion = move_and_slide(motion, UP)
+
+
+func horizontal_movement(delta):
 	if Input.is_action_pressed("move_right") and not Input.is_action_pressed("move_left"):
 		motion.x += ACCELERATION * delta
 		if motion.x > MAX_SPEED: motion.x = MAX_SPEED
+	
 	elif Input.is_action_pressed("move_left") and not Input.is_action_pressed("move_right"):
 		motion.x -= ACCELERATION * delta
 		if motion.x < -MAX_SPEED: motion.x = -MAX_SPEED
-	else:
-		motion.x = 0
 	
-	#Gravity
+	elif is_on_floor():
+		if motion.x > 0:
+			motion.x -= FRICTION * delta
+			if motion.x < 0: motion.x = 0
+		elif motion.x < 0:
+			motion.x += FRICTION * delta
+			if motion.x > 0: motion.x = 0
+
+
+func apply_gravity(delta):
 	if not is_on_floor():
 		motion.y += GRAVITY * delta
 	else:
 		motion.y = 0
-	
-	#Jump
+	#Añadir el caso cuando tocamos contra el techo...
+
+
+func jump():
 	if Input.is_action_pressed("jump") and is_on_floor():
 		motion.y = -JUMP_SPEED
-	
-	#Apply Movement
-	motion = move_and_slide(motion, UP)
+
+
+func _on_EnemyDetector_body_entered(body):
+	print("Enemy Detected! " + body.name)
