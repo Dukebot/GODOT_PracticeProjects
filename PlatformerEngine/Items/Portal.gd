cc835a7754c64aa44eb4e9a3a3e8e8d532cb5fc7
dukebot <danielfg1990@gmail.com>
2020-08-24 16:50:26 +0200

    Platformer Engine from scratch. Deleting the old one.

--- PlatformerEngine/Items/Portal.gd
+++
@@ -1,4 +0,0 @@
-extends Node2D
-
-func _on_Area2D_body_entered(body):
-	get_tree().call_group("GameState", "win_game")
--- PlatformerEngine/Player/Player.gd
+++ PlatformerEngine/Player/Player.gd
@@ -1,15 +1,13 @@
 extends KinematicBody2D
 
-signal animate
-
 const UP = Vector2(0, -1)
-const SPEED = 1000
-const GRAVITY = 6000 #7000 es un buen valor
-const JUMP_SPEED = 2500
-const WORLD_LIMIT = 4000
-const BOOST_MULTIPLIER = 2
+const ACCELERATION = 400
+const FRICTION = 400
+const GRAVITY = 800
+const MAX_SPEED = 300
+const JUMP_SPEED = 500
 
-var motion = Vector2(0, 0)
+var motion = Vector2()
 
 
 func _ready():
@@ -17,51 +15,26 @@ func _ready():
 
 
 func _physics_process(delta):
-	if position.y > WORLD_LIMIT:
-		get_tree().call_group("GameState", "end_game")
-	apply_gravity(delta)
-	jump()
-	move()
-	animate()
-	move_and_slide(motion, UP)
-
-
-func apply_gravity(delta):
-	if is_on_floor() and motion.y > 0:
-		motion.y = 0
-	elif is_on_ceiling():
-		motion.y = 1
-	else:
-		motion.y += GRAVITY * delta
-
-
-func jump():
-	if Input.is_action_pressed("jump") and is_on_floor():
-		motion.y = -JUMP_SPEED
-		$JumpSFX.play()
-
-
-func move():
-	if Input.is_action_pressed("left") and not Input.is_action_pressed("right"):
-		motion.x = -SPEED
-	elif Input.is_action_pressed("right") and not Input.is_action_pressed("left"):
-		motion.x = SPEED
+	
+	#Horizontal movement
+	if Input.is_action_pressed("move_right") and not Input.is_action_pressed("move_left"):
+		motion.x += ACCELERATION * delta
+		if motion.x > MAX_SPEED: motion.x = MAX_SPEED
+	elif Input.is_action_pressed("move_left") and not Input.is_action_pressed("move_right"):
+		motion.x -= ACCELERATION * delta
+		if motion.x < -MAX_SPEED: motion.x = -MAX_SPEED
 	else:
 		motion.x = 0
-
-
-func animate():
-	emit_signal("animate", motion)
-
-
-func hurt():
-	position.y -= 1
-	yield(get_tree(), "idle_frame")
-	motion.y = -JUMP_SPEED
-	$HurtSFX.play()
-
-
-func boost():
-	position.y -= 1
-	yield(get_tree(), "idle_frame")
-	motion.y = -JUMP_SPEED * BOOST_MULTIPLIER
+	
+	#Gravity
+	if not is_on_floor():
+		motion.y += GRAVITY * delta
+	else:
+		motion.y = 0
+	
+	#Jump
+	if Input.is_action_pressed("jump") and is_on_floor():
+		motion.y = -JUMP_SPEED
+	
+	#Apply Movement
+	motion = move_and_slide(motion, UP)
