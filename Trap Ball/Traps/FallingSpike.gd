6182056d0f756afce7b507577572f26ddc2c9344
dukebot <danielfg1990@gmail.com>
2020-09-02 18:54:21 +0200

    Trap Ball: Falling Spike and collision layers

---
+++ Trap Ball/Traps/FallingSpike.gd
@@ -0,0 +1,14 @@
+extends Area2D
+
+const FALLING_SPEED = 400
+var falling = false
+
+func _on_PlayerDetector_body_entered(body):
+	falling = true
+
+func _on_GroundDetectror_body_entered(body):
+	queue_free()
+
+func _process(delta):
+	if falling:
+		position.y += FALLING_SPEED * delta
