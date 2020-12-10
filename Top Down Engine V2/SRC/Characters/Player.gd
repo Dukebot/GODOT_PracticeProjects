77873f094cef811d620ed020f77d45cb436afe50
dukebot <danielfg1990@gmail.com>
2020-12-10 21:40:24 +0100

    Added footsteps to the top down engine.

--- Top Down Engine V2/SRC/Characters/Player.gd
+++ Top Down Engine V2/SRC/Characters/Player.gd
@@ -1,5 +1,7 @@
 extends Character
 
+onready var footsteps_sound = $Footsteps
+
 func _process(delta):
 	#do stuff here...
 	pass
@@ -7,7 +9,16 @@ func _process(delta):
 func _physics_process(delta):
 	var direction = InputManager.get_movement_direction()
 	movement.move(direction, delta)
+	footsteps_sound(movement.motion)
 
+
+func footsteps_sound(motion: Vector2):
+	if motion != Vector2.ZERO:
+		if not footsteps_sound.playing:
+			footsteps_sound.play()
+	else:
+		if footsteps_sound.playing:
+			footsteps_sound.stop()
 
 func _on_InteractionDetector_area_entered(area):
 	print("Interaction Detected")
