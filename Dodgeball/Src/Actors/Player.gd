248a77eb2fddae658ab6cbe74d521dca18142a21
dukebot <danielfg1990@gmail.com>
2021-01-08 18:27:07 +0100

    no message

--- Dodgeball/Src/Actors/Player.gd
+++ Dodgeball/Src/Actors/Player.gd
@@ -1,9 +1,16 @@
 class_name Player
 extends Actor
 
+signal no_lives()
+
+var lives := 3
+
 func _process(delta):
 	position = get_global_mouse_position()
 
-
 func _on_EnemyDetector_body_entered(body):
 	print("Enemy hit!")
+	body.queue_free()
+	lives -= 1
+	if lives <= 0:
+		emit_signal("no_lives")
