210212c14ab7466823bc305efb87c513a0e18103
dukebot <danielfg1990@gmail.com>
2021-04-20 18:54:07 +0200

    CubeDude: Animating the players.

--- UdemyCourse-CubeDude/Src/Players/Player.gd
+++ UdemyCourse-CubeDude/Src/Players/Player.gd
@@ -7,14 +7,42 @@ export (int, 1, 2) var player_id = 1
 
 var motion: Vector3
 
+onready var animation_player = $AnimationPlayer
+
+
 func _physics_process(delta):
 	move()
+	animate()
+	face_forward()
+
 
 func move():
 	var x = Input.get_action_strength("move_right_%s" % player_id) \
 			- Input.get_action_strength("move_left_%s" % player_id)
 	var z = Input.get_action_strength("move_down_%s" % player_id) \
 			- Input.get_action_strength("move_up_%s" % player_id)
-	var direction: Vector3 = Vector3(x, 0, z).normalized()
-	move_and_slide(direction * SPEED, UP_VECTOR)
 	
+	var direction = Vector3(x, 0, z).normalized()
+	motion = direction * SPEED
+	move_and_slide(motion, UP_VECTOR)
+
+
+func animate():
+	if motion.length() > 0:
+		animation_player.play("Arms Cross Walk")
+	else:
+		animation_player.stop()
+
+
+func face_forward():
+	if not motion.x == 0 or not motion.z == 0:
+		look_at(-motion, UP_VECTOR)
+
+
+
+
+
+
+
+
+
